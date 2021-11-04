422e4ba24c1521117e34ef135ae342808d1caa5a
devna <naelson.g.saraiva@gmail.com>
2021-11-04 09:57:52 -0400

    init doc readme

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\API\BaseController AS BaseController;
+
+class RegisterController extends BaseController
+{
+    /**
+     * Register raw on api
+     * it can return name and token to interactor vie e.g. via postman
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['name'] =  $user->name;
+        $success['token'] =  $user->createToken('rethink')->accessToken;
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+
+}
