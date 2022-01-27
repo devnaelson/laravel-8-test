5fa954a73aa9095bd457ff2c82d41028f0742401
= <naelson.g.saraiva@gmail.com>
2022-01-27 11:35:54 -0400

    up

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,28 @@ Route::get('/guzzle', function () {
     $res = Http::get('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL');
     echo $res->getStatusCode(); // 200
 
+?>
+    <!doctype html>
+    <html lang="en">
 
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <title>Hello, world!</title>
+    </head>
+
+    <body>
+        <h1>Hello, world!</h1>
+
+    </body>
+
+    </html>
+<?php
 
     $bought_value = "1.000.00";
 
